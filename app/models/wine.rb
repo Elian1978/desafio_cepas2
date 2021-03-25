7b0bb8381fbcad2a7b80a03a6490410bfb2b8499
Elian Rojas <impresosgosen@gmail.com>
2021-03-25 15:36:16 -0600

    Add Include

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,4 +1,10 @@
 class Wine < ApplicationRecord
-  has_many :wine_strains
+  #Permite borrar en cascada :destroy
+  has_many :wine_strains, dependent: :destroy
   has_many :strains, through: :wine_strains
+  # Para anidar un formulario dentro de wine con la tabla wine_strains debemos:
+  #no permitir porcentajes en blaco y borrar
+  accepts_nested_attributes_for(:wine_strains, reject_if: lambda{|a| a[:percentage].blank?}, allow_destroy: true)
+  #debemos agregar wine_strains_attributes en el wines.controller
+
 end
